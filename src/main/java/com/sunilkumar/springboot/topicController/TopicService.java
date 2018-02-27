c6dc2c170062975b59056753ed518972a28d2380
Sunilkumar Langatad <sunillangtad54@gmail.com>
2018-02-27 18:02:26 +0530

    Adds RequestMethod.PUT HTTP method

--- src/main/java/com/sunilkumar/springboot/topicController/TopicService.java
+++ src/main/java/com/sunilkumar/springboot/topicController/TopicService.java
@@ -26,4 +26,14 @@ public class TopicService {
     public void addTopic(Topic topic) {
         topics.add(topic);
     }
+
+    public void updateTopic(Topic topic, String id) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equalsIgnoreCase(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
 }
