b8c0d60f82880a9fee896e807e57b01625305259
Sunilkumar Langatad <sunillangtad54@gmail.com>
2018-02-28 23:19:23 +0530

    Adds an Interface which extends the CrudRepository

---
+++ src/main/java/com/sunilkumar/springboot/topicController/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.sunilkumar.springboot.topicController;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}
